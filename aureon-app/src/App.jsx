63c777a7dfddbedb0a97b6cd9b2b0976694f8a6f
gayathrim06 <gayathrimanojkottayam@gmail.com>
2026-09-01 18:45:22 +0530

    fix: remove floating ThemeToggleWidget pill as navbar already has theme toggle controls

--- aureon-app/src/App.jsx
+++ aureon-app/src/App.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
 import { AppRouter } from './pages/AppRouter';
-import { ThemeToggleWidget } from './components/common/ThemeToggleWidget';
 
 export function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <ThemeToggleWidget />
         <AppRouter />
       </AuthProvider>
     </ThemeProvider>
